2bbbbca491dd2d2f5f88c026b7c38d051c1df70f
r-naganuma <r.naganuma@roseaupensant.jp>
2019-11-22 00:41:02 +0900

    no message

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="output">
-    <div v-observe-visibility="{callback: visibilityChanged, once: true}" v-bind:class='{active:isVisible}' class="content fade_bottom" v-for="i in 6" :key=i>
-    <h3> {{ name }} </h3>
-    <img src="/hello_app/images/3.jpg">
-    <p> {{ text }} </p>
-  </div>
+    <h2 v-observe-visibility="{callback: visibilityChanged, once: true}" v-bind:class='{active:isVisible}' class="fade_in">OUTPUTS</h2>
+    <div v-observe-visibility="{callback: visibilityChanged, once: true, throttle: 2000}" v-bind:class='{active:isVisible}' class="output_box fade_bottom">
+      <div class="content" v-for="i in 6" :key=i>
+        <h3> {{ name }} </h3>
+        <img src="/hello_app/images/3.jpg">
+        <p> {{ text }} </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +18,7 @@ export default {
     return {
       name: 'タイトルエリア',
       text: 'このような意図で作成しました。ここをこのように工夫しました。ここが難しかったです。今後はこうゆう機能を追加したいです。'.repeat(3),
-      isVisible: true
+      isVisible: false
     }
   },
   methods: {
